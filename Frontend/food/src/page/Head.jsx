d90279e879a91d0e376350ddcf22c165761c38ba
Reni98 <renataoze1998@gmail.com>
2023-03-28 08:47:08 +0200

    Implement navbar

--- Frontend/food/src/page/Head.jsx
+++ Frontend/food/src/page/Head.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Button from "react-bootstrap/Button";
 import "bootstrap/dist/css/bootstrap.min.css";
-import HeadDesign from "../css/HeadDesign.css";
+import Design from "../css/HeadDesign.css";
 import Image from "../img/cooking.png";
 
 const Head = () => {
@@ -11,17 +11,6 @@ const Head = () => {
         <div className="left-side">
           <img src={Image} alt="" />
           <div className="circle_4"></div>
-
-          <div className="circle_5"></div>
-
-          <div className="circle_7"></div>
-
-          <div className="circle_8"></div>
-          <div className="circle_9"></div>
-          <div className="circle_10"></div>
-          <div className="circle_11"></div>
-          <div className="circle_12"></div>
-          <div className="circle_6"></div>
 
           <div className="circle"></div>
         </div>
@@ -43,7 +32,18 @@ const Head = () => {
         </div>
       </div>
 
-      <div className="shapes"></div>
+      <div className="shapes">
+        <div className="circle_5"></div>
+
+        <div className="circle_7"></div>
+
+        <div className="circle_8"></div>
+        <div className="circle_9"></div>
+        <div className="circle_10"></div>
+        <div className="circle_11"></div>
+        <div className="circle_12"></div>
+        <div className="circle_6"></div>
+      </div>
     </section>
   );
 };
