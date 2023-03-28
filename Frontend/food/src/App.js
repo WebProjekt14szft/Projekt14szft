e0415637cf0c25ff67c54568022813063d8044c9
Varró Ágnes <90404630+Axi20@users.noreply.github.com>
2023-03-28 12:58:25 +0200

    Fix merge issue / navbar

--- Frontend/food/src/App.js
+++ Frontend/food/src/App.js
@@ -1,24 +1,17 @@
 import "./App.css";
 import Head from "./page/Head";
-<<<<<<< HEAD
 import Body from "./page/Body";
-=======
 import { BrowserRouter } from "react-router-dom";
 import Mainroots from "./roots/Mainroots";
->>>>>>> e9876ca33603726948d64d42fb7a142fac808528
 
 function App() {
   return (
     <div className="App">
-<<<<<<< HEAD
-      <Head />
-      <Body />
-=======
       <BrowserRouter>
         <Mainroots />
         <Head />
+        <Body />
       </BrowserRouter>
->>>>>>> e9876ca33603726948d64d42fb7a142fac808528
     </div>
   );
 }
